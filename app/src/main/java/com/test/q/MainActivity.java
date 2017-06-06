77002929d5ecd02cad46185e2ed2519df623ed64
xgb <xguobin@139.com>
2017-06-06 13:24:39 +0800

    阿三顶顶顶顶顶顶6666666666666

--- app/src/main/java/com/test/q/MainActivity.java
+++ app/src/main/java/com/test/q/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //66asd                           asda速度
+        //66asd                         666  asda速度
     }//asdasd as
 }
