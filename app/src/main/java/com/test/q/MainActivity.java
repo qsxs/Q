edc340534d7aadf642adea6d1b53fa3c4a886e50
xgb <xguobin@139.com>
2017-06-06 13:28:20 +0800

    阿斯顿  顶顶顶

--- app/src/main/java/com/test/q/MainActivity.java
+++ app/src/main/java/com/test/q/MainActivity.java
@@ -10,5 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //66asd                         666  asda速度
-    }//asdasd as
+    }//asdasd as撒大大大大
 }
